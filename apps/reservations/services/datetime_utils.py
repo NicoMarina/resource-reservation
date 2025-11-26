08f4343ef7284998de8c32b37af9f43384ae680a
NicoMarina <mnicolauvalls@hotmail.com>
2025-11-26 17:29:06 +0100

    [REFACTOR] Rename create_reservation to create_reservation_service and extract input normalization

---
+++ apps/reservations/services/datetime_utils.py
@@ -0,0 +1,19 @@
+from datetime import datetime, time
+
+
+def normalize_reservation_inputs(date_str, start_time=None, end_time=None):
+    """Convert strings to date and time objects, assigning default values if missing."""
+    date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    default_start = time(8, 0)
+    default_end = time(20, 0)
+
+    if isinstance(start_time, str):
+        start_time = datetime.strptime(start_time, "%H:%M").time()
+    if isinstance(end_time, str):
+        end_time = datetime.strptime(end_time, "%H:%M").time()
+
+    start_time = start_time or default_start
+    end_time = end_time or default_end
+
+    return date, start_time, end_time
